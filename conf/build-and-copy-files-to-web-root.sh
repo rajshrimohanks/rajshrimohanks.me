f0e4e1270e24579b3ac60ca2864b0d2215872eba
Rajshri Mohan K S <rajshri.m@iqzsystems.com>
2019-01-26 18:08:42 +0530

    updated conf

--- conf/build-and-copy-files-to-web-root.sh
+++ conf/build-and-copy-files-to-web-root.sh
@@ -5,6 +5,7 @@ set -e
 hugo
 
 # Copy files to webroot
+set -e
 rsync -avh ./public/. /var/www/rajshrimohanks.me/ \
   --chown www-data:www-data \
   --progress \
---
+++ conf/update-nginx-conf.sh
@@ -0,0 +1,3 @@
+#1/bin/bash
+
+cp -v ./conf/nginx/rajshrimohanks.me.nginx.conf /etc/nginx/sites-available/
